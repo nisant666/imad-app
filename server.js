086e7d04f1445da280170dd06ecfe2dc4a7108ac
nisant666 <30744205+nisant666@users.noreply.github.com>
2017-08-24 05:55:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ function hash (input, salt){
     return hashed.toString('hex'); 
 }
 
-app.get('hash/:input', function (req, res){
+app.get('/hash/:input', function (req, res){
     var hashedString = hash(req.params.input, 'this-is-salt-string');
     res.send(hashedString);
     
